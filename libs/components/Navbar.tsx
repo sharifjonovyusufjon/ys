80081bc096f2ee770ade7b156a5e6eda48895bf5
sharifjonovyusufjon <yusufjon6727@gmail.com>
2026-09-23 16:39:01 +0900

    fix: updating publish

--- libs/components/Navbar.tsx
+++ libs/components/Navbar.tsx
@@ -6,7 +6,25 @@ const Navbar = () => {
   if (device === "mobile") {
     return (
       <>
-        <div>Navbar (Mobile)</div>
+        <div
+          style={{
+            width: "100%",
+            height: "90px",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              width: "390px",
+              height: "50px",
+              border: "1px solid #C4C4C4",
+              borderRadius: "10px",
+            }}
+          ></div>
+        </div>
       </>
     );
   } else {
